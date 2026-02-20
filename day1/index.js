cf6eabb536ba6bc5eb9ceb3885be71b564c27dee
sachinp <sachinp@iitbhilai.ac.in>
2026-02-20 20:22:50 +0530

    .

---
+++ day1/index.js
@@ -0,0 +1,31 @@
+// Google sheet
+// Our goal:
+// 1. cell's value editing.
+// 2. formulla writting section
+// 3. basic formula suppport
+// 4. Basic formatting of the values.
+// 5. automatic recalculation of the values when the dependent cells are updated.
+// 6. tracking deps.
+
+// what would be our basic layout or architecture.(a + b)
+
+// toolbar: formatting buttons, formula bar, address also.
+// row header: A , B , C......Z
+// column header: 1, 2, 3, 4, 5.....
+// cell: the intersection of row and column header.
+
+// how user can interact and use the application.
+// 1. click on cell to edit its value.
+// 2. enter in the formula bar and write.
+// Formula format and syntax, destination cell;
+//  cell can be rep as A1, B2, C3, D4, E5....Z1000
+// i wana add A1 and B1 value.
+// I will have to click in the destination cell (where the result will be stored).
+// Then write the formula in the formula bar like this:A1+B1
+// hit enter, u will get result in the destination cell.
+
+// we will have some formatting buttons
+//  click on the cell whose value u want to format.
+// then click on the respective formatting button.
+
+
